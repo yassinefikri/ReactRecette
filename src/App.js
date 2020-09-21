581797fdfc19366d7e5225b3bb9aad5e4f3a2e21
Yassine Fikri <ysn.fikri@gmail.com>
2020-09-21 19:20:20 +0100

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Recette from './Components/Recette.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    recettes : [],
+    newRecette : ''
+  }
+
+  handleNewRecetteSubmit = (e) =>{
+    e.preventDefault();
+    if(this.state.newRecette.length>0){
+      const recettes = [...this.state.recettes];
+      const id = new Date().getTime();
+      const nom = this.state.newRecette;
+      const ings = [];
+      recettes.unshift({ id, nom , ings })
+      this.setState({recettes , newRecette : ''});
+    }
+  }
+
+  handleNewRecetteChange = (e) =>{
+    this.setState({newRecette : e.currentTarget.value});
+  }
+
+  editRecette = (recetteEdited) => {
+    const recettes = [...this.state.recettes];
+    const index = recettes.findIndex(function(recette){
+      return recette.id == recetteEdited.id
+    });
+    recettes[index]= recetteEdited;
+    this.setState({recettes});
+  }
+
+  deleteRecette = (id) => {
+    const recettes = [...this.state.recettes];
+    const index = recettes.findIndex(function(recette){
+      return recette.id == id
+    });
+    recettes.splice(index,1);
+    this.setState({recettes});
+  }
+
+  render(){
+    return (
+      <div className="App container">
+        <br/>
+        <h1 className="display-4">Ajouter une Recette</h1>
+        <br/>
+        <form onSubmit={this.handleNewRecetteSubmit}>
+          <div className="form-group">
+            <input type="text" placeholder="Nom de la Recette" className="form-control" value={this.state.newRecette} onChange={this.handleNewRecetteChange} />
+          </div>
+          <button className="btn btn-success">Ajouter</button>
+        </form>
+        <hr/>
+        <h1 className="display-4">Recettes</h1>
+        <div className="row row-cols-1 row-cols-md-3">
+          {this.state.recettes.map(recette => (
+            <Recette detail={recette} editRecette={this.editRecette} deleteRecette={this.deleteRecette} key={recette.id} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Recette.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+class Recette extends React.Component {
+
+    state = {
+        recette : this.props.detail,
+        isEdit : false,
+        newIng : ''
+    }
+
+    handleEditButton = () => {
+        this.setState({ isEdit : !this.state.isEdit});
+    }
+
+    handleNewIngChange = (e) =>{
+        this.setState({newIng : e.currentTarget.value});
+    }
+
+    handleNewIngSubmit = (e) =>{
+        e.preventDefault();
+        if(this.state.newIng.length>0){
+            const recette = this.state.recette;
+            const id = new Date().getTime();
+            const name = this.state.newIng;
+            recette.ings.push({id, name});
+            this.setState({newIng : ''});
+            this.props.editRecette(recette);
+        }
+    }
+
+    deleteIng = (ingid) =>{
+        const recette = this.state.recette;
+        const index = recette.ings.findIndex(function(ing){
+            return ing.id==ingid
+        })
+        recette.ings.splice(index,1);
+        this.props.editRecette(recette);
+    }
+
+    deleteRecette = () => {
+        this.props.deleteRecette(this.state.recette.id);
+    }
+
+    render() {
+        return (
+            <div className="col mb-4">
+              <div className="card" style={{width: "18rem"}}>
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.recette.nom}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Ingrédients</h6>
+                    <ul className="list-group list-group-flush">
+                        {this.state.recette.ings.map(ing => (
+                            <li className="list-group-item" key={ing.id}>{ing.name} <button onClick={() => this.deleteIng(ing.id)}>X</button></li>
+                        ))}
+                    </ul>
+                    { this.state.isEdit ? (<div><hr/><form onSubmit={this.handleNewIngSubmit}>
+                                                <div className="form-group">
+                                                    <input type="text" placeholder="Nom de l'ingrédient" className="form-control" value={this.state.newIng} onChange={this.handleNewIngChange} />
+                                                </div>
+                                                <button className="btn btn-success">Ajouter</button>
+                                                </form><hr/></div>) 
+                                            : (<div></div>) 
+                    }
+                    <div className="mt-3 d-flex flex-wrap justify-content-around">
+                        { this.state.isEdit ? (<button onClick={this.handleEditButton} className="btn btn-success">Enregister</button>) : (<button onClick={this.handleEditButton} className="btn btn-warning">Modifier</button>) }
+                        <button onClick={this.deleteRecette} className="btn btn-danger">Supprimer</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+        );
+    }
+}
+
+export default Recette;
